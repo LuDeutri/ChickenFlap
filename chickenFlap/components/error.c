d0fa528ba129ad2b5b663b7528e5ef4318626951
luca126 <lucadeutrich@gmail.com>
2023-11-02 20:50:31 +0100

    spelling fix

--- chickenFlap/components/error.c
+++ chickenFlap/components/error.c
@@ -100,7 +100,7 @@ void checkDisplayInit(){
 }
 
 void checkBatterySOC(){
-	// If no batterie is used stop error checking
+	// If no battery is used stop error checking
 	#if !(defined(USE_BATTERY) && defined(ENABLE_BATTERY_ERR_CHECK))
 		return;
 	#endif
